1177eb7d2ba91b3f1bf7e9c258ffd1517982215c
mayabarlev10-hub <maya.barlev10@gmail.com>
2025-11-19 16:58:01 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -21,18 +21,8 @@ def split_at_first_digit(formula):
 
 
 def split_before_each_uppercases(formula):
-    """
-    מפצלת מחרוזת לרשימת מחרוזות, כאשר הפיצול מתבצע לפני כל אות רישית חדשה.
-    הפיצול אינו מתבצע בתחילת המחרוזת.
-    
-    דוגמה: "IronOxide" -> ['Iron', 'Oxide']
-    דוגמה: "someVariable" -> ['some', 'Variable']
-    """
-    
-    # הדפוס: r'(?<!^)(?=[A-Z])'
-    # (?<!^) : Negative Lookbehind - מוודא שהפיצול אינו מתבצע בתחילת המחרוזת.
-    # (?=[A-Z]): Positive Lookahead - מחפש מיקום שבא אחריו אות רישית, ומחליף אותו ברווח.
+
     parts_string = re.sub(r'(?<!^)(?=[A-Z])', r' ', formula)
     
-    # פיצול המחרוזת לפי הרווחים שהוספנו
-    
+
+    return parts_string.split(' ')
